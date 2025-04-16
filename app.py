86dfea40bac8b49fbda5f0989e2157e5e015cc80
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-04-16 15:02:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,10 @@ st.session_state["page"] = page
 # Home Page
 if page == "Home":
     st.title("👩🏻‍🔬 Barran Group IM-MS Processing Tools")
-    st.subheader("←←← Navigate to the tool you need from the sidebar.")
-    st.markdown('''This is a work in progress, so there are a few things to note:   
-    1. I haven't validated this - sanity check your results, don't trust the outputs for important things.   
-    2. If the site is down it is because I'm working on it, either find a previous version on my github or just ask me and I'll sort it!   
-    3. I encourage you to make improvements to this! Please make a new branch and do your thing, then open a pull request and I will likely accept.''')
+    st.subheader("← Navigate to the tool you need from the sidebar.")
+    st.markdown('''This is a work in progress, so there are a few things to note: I haven't validated this - sanity check your results, don't trust the outputs for important things. If the site is down it is because I'm working on it, either find a previous version on my github or just ask me and I'll sort it! I encourage you to make improvements to this - please make a new branch and do your thing, then open a pull request and I will likely accept.
+    
+    Other than that, I hope you enjoy. Let me know if you need any help (ana.bathalen@manchester.ac.uk or slack)!''')
 
 # Fit Gaussians to Data Page
 elif page == "Fit Gaussians to Data":
